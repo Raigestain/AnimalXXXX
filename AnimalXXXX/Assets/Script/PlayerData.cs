9c4dd0c49c9d0d5ff1011963548d89760086fe69
Yeipi <jpgomey@gmail.com>
2020-02-01 21:13:50 -0600

    Agregado script PlayerData. -Incluye solo la función para obtener una lista de animales por area.

--- AnimalXXXX/Assets/Script/PlayerData.cs
+++ AnimalXXXX/Assets/Script/PlayerData.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class PlayerData : MonoBehaviour
 {
-    public static int m_chickenCount = 0;
+    public static int m_barnDurability = 0;
+    public static int m_barnLife = 0;
+
 
     // Start is called before the first frame update
     void Start()
@@ -17,14 +19,27 @@ public class PlayerData : MonoBehaviour
     {
         
     }
-
-    public static void AddChickens(uint number)
+    
+    /// <summary>
+    /// Regresa una lista de animales que se encuentren en el área indicada.
+    /// </summary>
+    /// <param name="pos">Punto central del área</param>
+    /// <param name="radius">Radio del área</param>
+    /// <returns>Lista de animales</returns>
+    public static List<Animal> GetAnimalsByArea(Vector3 pos, float radius)
     {
-        m_chickenCount += (int)number;
-    }
+        List<Animal> animalList = new List<Animal>();
+        var animals = GameObject.FindObjectsOfType<Animal>();
 
-    public static void ExpendChickens(uint number)
-    {
-        m_chickenCount -= (int)number;
+        foreach (var animal in animals)
+        {
+            float distance = (animal.transform.position - pos).magnitude;
+            if (distance < radius)
+            {
+                animalList.Add(animal);
+            }
+        }
+        return animalList;
     }
+    
 }
